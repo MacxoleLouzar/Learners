10a29f22cf675ce1e805650e408a5b2bc0800099
Masixole <masixole.mtsikini@dadevs.co.za>
2023-03-07 15:48:20 +0200

    Landing Page 50% done

--- frontend/src/Pages/Landing.jsx
+++ frontend/src/Pages/Landing.jsx
@@ -1,29 +1,74 @@
 import React from "react";
-import Col from "react-bootstrap/Col";
-import Form from "react-bootstrap/Form";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 
 const Landing = () => {
   return (
     <>
-      <Form className="form">
-        <Row>
-          <Col className="Col-Pwd">
-          <card>PWD</card>
-          </Col>
-          <Col className="Col-FS">
-          <div>FS</div>
-          </Col>
+      <div className="form">
+        <Row className="Row">
+            <div className="Card" style={{ width: "25rem"}}>
+              <Card.Img
+               className="Img"
+               src="https://images.unsplash.com/photo-1573495626828-c43042e94374?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"                
+              />
+              <Card.Body>
+                <Card.Title>PWD</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </div>
+
+            <div className="Card" style={{ width: "25rem"}}>
+              <Card.Img
+               className="Img"
+                src="https://images.unsplash.com/photo-1457305237443-44c3d5a30b89?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8NHw0ODYzMTY2M3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
+              <Card.Body>
+                <Card.Title>Full Stack</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </div>
         </Row>
-        <Row>
-          <Col className="Col-DS">
-          <div>DS</div>
-          </Col>
-          <Col className="Col-SF">
-          <div>SF</div>
-          </Col>
+
+        <Row className="Row">
+            <div className="Card" style={{ width: "25rem"}}>
+              <Card.Img
+                className="Img"
+                src="https://images.unsplash.com/photo-1531482615713-2afd69097998?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8Mnw0ODYzMTY2M3x8ZW58MHx8fHw%3D&auto=format&fit=crop&w=500&q=60"/>
+              <Card.Body>
+                <Card.Title>Data Science</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </div>
+      
+          
+            <div className="Card" style={{ width: "25rem"}}>
+              <Card.Img className="Img"
+                src="https://images.unsplash.com/photo-1580894908361-967195033215?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxjb2xsZWN0aW9uLXBhZ2V8MTB8NDg2MzE2NjN8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60"
+              />
+              <Card.Body>
+                <Card.Title>Graduate Elite</Card.Title>
+                <Card.Text>
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </Card.Text>
+                <Button variant="primary">Go somewhere</Button>
+              </Card.Body>
+            </div>
         </Row>
-      </Form>
+      </div>
     </>
   );
 };
