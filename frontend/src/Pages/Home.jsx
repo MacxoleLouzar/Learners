216c95e458ab776f4a9a43438ac05096818dd7bf
Masixole <masixole.mtsikini@dadevs.co.za>
2023-03-08 16:15:49 +0200

    Home Page 50% done no functionality

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,9 +1,117 @@
-import React from 'react'
+import React from "react";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <>
+      <div className="container">
+        <h3 className="title">Home</h3>
+        <div className="form-group row">
+          <div className="col-sm-3 offset-8">
+            <input
+              type="filter"
+              className="form-control"
+              id="filter"
+              placeholder="search learner"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary col-sm-1">
+            Filter
+          </button>
+        </div>
+        <div className="form">
+          <div className="Row">
+            <div className="card">
+              <img className="" src=" https://images.unsplash.com/photo-1522529599102-193c0d76b5b6?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDd8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">John Doe</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img className="" src="https://images.unsplash.com/photo-1523464862212-d6631d073194?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTZ8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Carrol Mathew</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img className="" src="https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mjh8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Angel Smith</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="Row">
+          <div className="card">
+              <img className="" src="https://images.unsplash.com/photo-1481437642641-2f0ae875f836?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Jele Sihle</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img className="" src="https://images.unsplash.com/photo-1485546246426-74dc88dec4d9?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NjV8fHBlb3BsZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=500&q=60" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Zulu Xolani</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+            <div className="card">
+              <img className="" src="https://images.unsplash.com/photo-1537511446984-935f663eb1f4?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="" />
+              <div className="card-body">
+                <h5 className="card-title">Nkosi Luvo</h5>
+                <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+                <p className="card-text">
+                  Some quick example text to build on the card title and make up
+                  the bulk of the card's content.
+                </p>
+                <a href="home" className="btn btn-primary">
+                  Go somewhere
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="load">.....Load</div>
+      </div>
+    </>
+  );
+};
 
-export default Home
+export default Home;
